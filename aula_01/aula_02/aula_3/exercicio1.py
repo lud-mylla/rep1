76f0bae42ab889825d8b96a88ab633bb91478ad4
lud-mylla <Ludmylla100608@gmail.com>
2025-04-15 14:57:29 +0000

    primeiro commit

---
+++ aula_01/aula_02/aula_3/exercicio1.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome completo: ")
+x = nome.split()
+primeironome = x[0]
+
+print("Seja bem-vindo(a) ao Python," + primeironome)
+
---
+++ aula_01/aula_02/aula_3/exercicio2.py
@@ -0,0 +1,5 @@
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a primeira nota: "))
+
+media = (nota1 * 2 + nota2 * 3)/5 
+print(f"Sua Média é = {int(media)}")
